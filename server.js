f24a9e06185b43ed2c8db1c54bbdab2d3217a6d6
AD001\z002txtf <codruta.ene@siemens.com>
2019-01-09 15:58:25 +0200

    added delete

--- server.js
+++ server.js
@@ -60,6 +60,19 @@ app.post('/todos', function(req, res){
 	res.json(body);
 });
 
+//DELETE /todos/:id
+app.delete('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var foundTodo  = _.findWhere(todos, {id: todoId});
+	if (!foundTodo){
+		res.status(404).json({"error": "no todo found with that id"});
+	}
+	else {
+		todos = _.without(todos, foundTodo);
+		res.json(foundTodo);
+	}
+});
+
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT);
 })
