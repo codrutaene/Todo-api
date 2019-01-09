c572c102f38bb7b52117079ed8ee50066ea9498d
AD001\z002txtf <codruta.ene@siemens.com>
2019-01-09 17:06:02 +0200

    added completed filter

---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+// var person = {
+// 	name: 'Cucu',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// body...
+// 	obj.age = 34;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//array ex
+var array = [15, 27];
+
+function addGrade(arr){
+	arr.push(13);
+	debugger;
+}
+
+addGrade(array);
+console.log(array);
