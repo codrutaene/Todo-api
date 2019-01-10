6df074e4f5acd5858ad6f1fca1ddc252941f9550
AD001\z002txtf <codruta.ene@siemens.com>
2019-01-10 18:42:09 +0200

    added DELETE /todos with sequelize

--- server.js
+++ server.js
@@ -74,17 +74,21 @@ app.post('/todos', function(req, res) {
 //DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var foundTodo = _.findWhere(todos, {
-		id: todoId
-	});
-	if (!foundTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	} else {
-		todos = _.without(todos, foundTodo);
-		res.json(foundTodo);
-	}
+	
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted){
+		if (rowsDeleted === 0){
+			res.status(404).json({
+				error: "no todo with this id"});
+		}else{
+			res.status(204).send();
+		}
+	},function(e){
+		res.status(500).send();
+	});	
 });
 
 
